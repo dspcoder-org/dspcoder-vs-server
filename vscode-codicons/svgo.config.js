7308ca75077ed6a896589fffba4b9b7ad0caed53
anshul <anshulmaurya27@gmail.com>
2025-06-28 14:20:23 -0400

    adding codicon dev

---
+++ vscode-codicons/svgo.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    plugins: [
+        // Remove the plugin that strips 'fill' attributes
+        // {
+        //     name: 'removeAttrs',
+        //     params: {
+        //         attrs: 'fill'
+        //     }
+        // },
+        // Remove the plugin that forces 'fill: currentColor'
+        // {
+        //     name: 'addAttributesToSVGElement',
+        //     params: {
+        //         attributes: [
+        //             {
+        //                 fill: 'currentColor'
+        //             }
+        //         ]
+        //     }
+        // }
+    ]
+}
